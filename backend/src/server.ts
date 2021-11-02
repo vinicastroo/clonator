f63ccc53287864d884156c4fea7b1bbb2e8eadc7
Lucas Valente <lucas.valente@asksuite.com>
2021-11-01 21:32:28 -0300

    [BACKEND] - Added cors.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,5 @@
-import 'reflect-metadata';
-
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 
 import 'express-async-errors';
 
@@ -11,6 +10,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
